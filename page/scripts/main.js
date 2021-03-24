7fac86f70bb09cc9fb0726cbebea0c35b0730022
sebastian carter <carteseb000@gmail.com>
2021-03-23 20:41:37 -0400

    n/a

---
+++ page/scripts/main.js
@@ -0,0 +1,2 @@
+$('body').css('background-color', 'black');
+$('button').add('button').css('value', 'test');
---
+++ server.js
@@ -0,0 +1,17 @@
+console.log('server has started!');
+
+const { request, response } = require('express');
+var express = require('express');
+
+var app = express();
+
+app.get('/api', (request, response) => {
+    response.send('Hello World');
+});
+
+var server = app.listen(3000, listening());
+
+function listening() {
+    console.log('listening...');
+}
+app.use(express.static('page'));
